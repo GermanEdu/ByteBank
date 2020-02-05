e27e54184a824d3c5ce9f70d80c2f0404a534bbd
EVGERMANO <EVGERMANO@C-E0803>
2020-02-05 19:33:18 -0300

    sobregargas

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ByteBank.Funcionarios;
+using System;
 
 namespace ByteBank
 {
@@ -10,6 +7,36 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario Algum = new Funcionario();
+
+            Algum.Nome = "Carlos";
+            Algum.Salario = 2000;
+
+            gerenciador.Registrar(Algum);
+
+
+            Diretor Eduardo = new Diretor();
+            Eduardo.Nome = "Eduardo";
+            Eduardo.Salario = 5000;
+            gerenciador.Registrar(Eduardo);
+
+
+            Console.WriteLine(Algum.Nome);
+            Console.WriteLine(Algum.GetBonificacao());
+            Console.WriteLine();
+            Console.WriteLine(Eduardo.Nome);
+            Console.WriteLine(Eduardo.GetBonificacao());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Total de bonificação: " + gerenciador.getTotalBonificacao());
+
+
+            Console.ReadLine();
+
+
         }
     }
 }
